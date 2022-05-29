538b82a7376f331de8f4e237ef3a4bfd398ed67c
ashikhmareva <ashikhmareva@openintegration.inc>
2022-05-29 17:52:46 +0300

    turn CommonUtils to object

--- src/test/kotlin/com/shikhmareva/utils/CommonUtils.kt
+++ src/test/kotlin/com/shikhmareva/utils/CommonUtils.kt
@@ -1,17 +1,15 @@
 package com.shikhmareva.utils
 
 import io.qameta.allure.Attachment
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.TakesScreenshot
 import org.openqa.selenium.OutputType
+import org.openqa.selenium.TakesScreenshot
+import org.openqa.selenium.WebDriver
 
-class CommonUtils {
+object CommonUtils {
 
-    companion object {
-        @Attachment
-        fun saveScreenShot(driver: WebDriver): ByteArray {
-            return (driver as TakesScreenshot).getScreenshotAs(OutputType.BYTES)
-        }
+    @Attachment
+    fun saveScreenShot(driver: WebDriver): ByteArray {
+        return (driver as TakesScreenshot).getScreenshotAs(OutputType.BYTES)
     }
 
 }
